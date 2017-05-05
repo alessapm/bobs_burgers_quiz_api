987ba4503851bebd2bd472651bbbdfe45b362339
Alessa Messineo <apmessineo@gmail.com>
2017-05-04 21:25:07 -0400

    establish characters controller

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,20 @@
+class CharactersController < ApplicationController
+
+def index
+  characters = Character.all
+  render :json => characters, status: 200
+end
+
+def show
+  character = Character.find(params[:id])
+  render :json => character, status: 200
+end
+
+# bob = 3, linda = 4, tina = 5, gene = 6, louise = 7
+
+
+
+
+end
+
+
